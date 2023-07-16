40ce14dfc598466a58e3b7f54a77c13c1ce71e75
Mahesward <maheaswerd@gmail.com>
2023-07-16 19:44:34 +0530

    [updates] added copy to clipboard in link-shortener

--- src/components/link-shortener/link-shortener.tsx
+++ src/components/link-shortener/link-shortener.tsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './link-shortener.scss';
 import NavBar from '../navbar/navbar';
 
 const LinkShortener = () => {
   const [URL, setURL] = useState('');
   const [shortURL, setShortURL] = useState('');
+  const [copied, setCopied] = useState(false);
 
   const handleSubmit = async () => {
     const res = await axios.post(`https://api.shrtco.de/v2/shorten?url=${URL}`);
@@ -24,14 +26,26 @@ const LinkShortener = () => {
             className="link-shortener-input"
             placeholder="Enter URL"
             value={URL}
-            onChange={(e) => setURL(e.target.value)}
+            onChange={(e) => {
+              setURL(e.target.value);
+              setCopied(false);
+            }}
           />
           <button className="link-shortener-button" onClick={handleSubmit}>
             Submit
           </button>
         </div>
 
-        {shortURL && <p className="short-url">{shortURL}</p>}
+        {shortURL && (
+          <CopyToClipboard text={shortURL} onCopy={() => setCopied(true)}>
+            <div className="link-shortener-form">
+              <input type="text" name="shorturl" className="link-shortener-input" value={shortURL} disabled />
+              <button className="link-copy-button">
+                <span>{copied ? <span>Copied 🙄</span> : <span>Copy 📋</span>}</span>
+              </button>
+            </div>
+          </CopyToClipboard>
+        )}
       </div>
     </>
   );
